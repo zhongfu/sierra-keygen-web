8494585558fe6d22b2298c4ad62781b6e3868ce9
zhongfu <me@zhongfu.li>
2023-07-24 08:44:21 +0800

    refactor: remove hcaptcha vars, skip uploading commented out secrets

--- make_secrets.sh
+++ make_secrets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # lmao
-echo {$(cat .dev.vars | sed "s|=| |" | awk '{print "\"" $1 "\":\"" $2 "\","}' | tr -d "\n" | sed "s|,$||")} > secrets.json
+echo {$(cat .dev.vars | grep -v '^#' | sed "s|=| |" | awk '{print "\"" $1 "\":\"" $2 "\","}' | tr -d "\n" | sed "s|,$||")} > secrets.json
